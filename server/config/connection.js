4f83a18dfe5e969a9d115747ce5dddb6629529f6
LisaTLe <lisa.le1127@gmail.com>
2022-09-26 21:54:47 -0600

    edit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+console.log(process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
